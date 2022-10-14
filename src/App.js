b38a509ed567d0acc0442d0f0110a9e1eda15c0c
matuteduro <matias_duro@hotmail.com>
2022-10-14 20:13:35 -0300

    skills

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Navbarr from './components/Navbarr';
 import Banner from './components/Banner';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Skills from './components/Skills';
 
 function App() {
   return (
     <div className="App">
       <Navbarr/>
       <Banner />
+      <Skills/>
     </div>
   );
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,64 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import html from "../assets/icons/html.png";
+import css from "../assets/icons/css.png";
+import javascript from "../assets/icons/javascript.png";
+import react from "../assets/icons/react.png";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
-const Skills = () => {
+
+export const Skills = () => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+
   return (
-    <div>Skills</div>
+    <section className="skill" id="skills">
+        <div className='bar'></div>
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <div className="skill-bx wow zoomIn">
+                        <h2>Skills</h2>
+                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                            <div className="item">
+                                <img src={html} alt="Image" />
+                                <h5>HTML</h5>
+                            </div>
+                            <div className="item">
+                                <img src={css} alt="Image" />
+                                <h5>CSS</h5>
+                            </div>
+                            <div className="item">
+                                <img src={javascript} alt="Image" />
+                                <h5>Javascript</h5>
+                            </div>
+                            <div className="item">
+                                <img src={react} alt="Image" />
+                                <h5>React</h5>
+                            </div>
+                        </Carousel>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
