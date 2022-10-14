572c55c8796ae83dce9776ab0999e686f4a047fe
matuteduro <matias_duro@hotmail.com>
2022-10-14 18:56:40 -0300

    banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbarr from './components/Navbarr';
+import Banner from './components/Banner';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
       <Navbarr/>
+      <Banner />
     </div>
   );
 }
