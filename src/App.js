896b8a6cfaeb7887668f8d29507c1378a92bdd14
matuteduro <matias_duro@@hotmail.com>
2022-10-15 15:16:39 -0300

    uploads

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Navbarr from './components/Navbarr';
 import Banner from './components/Banner';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Skills from './components/Skills';
+import Projects from './components/Projects'
+import Contact from './components/Contact';
+import Footer  from './components/Footer';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <Navbarr/>
       <Banner />
       <Skills/>
+      <Projects/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,90 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/StartUp.png";
+import projImg2 from "../assets/img/landing.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+import React from 'react'
+
+const Projects = () => {
+
+  const project = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+  ];
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>This are a few projects I did, most of them are made using React, Node Js, MongoDb and I will be uploading more to showcase my knowledge</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          project.map((projec, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...projec}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="section">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={''}></img>
+    </section>
+  )
+}
+
+export default Projects
